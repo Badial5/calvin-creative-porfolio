8c29cde2201d9f6a87f6dd7bdd32460e92b19119
CalvinWilliams2022 <calvinwilliams2022@gmail.com>
2022-08-20 09:09:35 +0000

    Added the FAQ Section

--- src/pages/about/about-us.jsx
+++ src/pages/about/about-us.jsx
@@ -4,12 +4,17 @@ import ServiceSection from '../../components/service/serviceSection'
 
 import AboutSection from '../../components/about/about-section.component'
 
+import FaqSection from '../../components/faq/faqSection.component'
+
+
 const AboutUs = () => {
   return (
     <div>
       <AboutSection />
 
       <ServiceSection />
+
+      <FaqSection />
     </div>
   )
 }
