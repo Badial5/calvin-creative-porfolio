f5814325fd5f3b6460d1f74675b6946cc0bb158f
CalvinWilliams2022 <calvinwilliams2022@gmail.com>
2022-08-23 13:23:48 +0000

    Navbar Animation Added

--- src/Routes/Navigation/nav.component.jsx
+++ src/Routes/Navigation/nav.component.jsx
@@ -2,14 +2,17 @@
 
 
 //Navigation components
-import { Outlet, Link } from "react-router-dom"
-
+import { motion } from "framer-motion";
+import { Outlet, Link, useLocation } from "react-router-dom"
 
 //import styled component
 import styled from 'styled-components';
 
 
 const Nav = () => {
+
+    const {pathname} = useLocation();
+
   return (
     <>
     <StyledNav>
@@ -17,14 +20,26 @@ const Nav = () => {
         <Link id="logo" to='/' >Digital Star</Link>
 
         <ul>
-
-            <Link className="li" to='work'>
+            <li>
+            <Link  to='work'>
                 Our Work
             </Link> 
+            <Line transition={{duration: 0.76}} 
+            initial={{ width: "0%"}} animate={{width: pathname === "/work" ? "50%" : "0%" }} 
+            />
+            
+            </li>
 
-            <Link className="li" to='contact'>
+
+            <li>
+            <Link  to='contact'>
                 Contact Us
             </Link>
+            <Line transition={{duration: 0.76}} 
+            initial={{ width: "0%"}} animate={{width: pathname === "/contact" ? "50%" : "0%" }} 
+            />
+            
+            </li>
 
         </ul>
 
@@ -47,16 +62,16 @@ const Nav = () => {
 // Navigation Styles component
 
 const StyledNav = styled.nav`
-    min-height: 10vh;
-    display: flex;
-    margin: auto;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem 10rem;
-    background: #282828;
-    position: sticky;
-    top: 0;
-    z-index: 10;
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  background: #282828;
+  padding: 1rem 10rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
 
     a{
         color: white;
@@ -74,34 +89,36 @@ const StyledNav = styled.nav`
         font-weight: lighter;
     }
 
-    .li {
+    li {
         padding-left: 10rem;
         position: relative;
     }
 
-    @media (max-width: 1300px){
-
-        flex-direction: column;
-        padding: 2rem 1rem;
-
-        #logo {
-            display: inline-block;
-            /* margin: 2rem; */
-        }
-
-        ul {
-            padding: 2rem;
-            justify-content: space-around;
-            width: 100%;
-
-            li{
-                padding: 0;
-            }
-        }
+    @media (max-width: 1500px) {
+    flex-direction: column;
+    padding: 2rem 0rem;
+    ul {
+      padding: 2rem;
+      justify-content: space-around;
+      width: 100%;
+      li {
+        padding: 0;
+      }
     }
-
+  }
 `;
 
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background: #23d997;
+  width: 5%;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+  @media (max-width: 1500px) {
+    left: 0%;
+  }
+`;
 
 
 
